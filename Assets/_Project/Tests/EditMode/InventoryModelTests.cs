c100ac92b31dcbfb6d52fcf95c281fb59c51af82
Anis <>
2025-12-01 12:24:51 +0200

    Add InventoryModelTests Unit Test script
    - In the unit test script, we have: SetUp, AddNewItem test, AddSameItemTwice test, RemoveItem test, and SwapItems test.

---
+++ Assets/_Project/Tests/EditMode/InventoryModelTests.cs
@@ -0,0 +1,93 @@
+using NUnit.Framework;
+using Project.Features.Inventory.Domain;
+using UnityEngine;
+
+namespace Project.Tests
+{
+    public class InventoryModelTests
+    {
+        private InventoryModel m_InventoryModel;
+        private InventoryItemSO m_PotionItem; 
+        private InventoryItemSO m_SwordItem;
+        
+        // Since it has Set up annotation, it will run with every test
+        // This is first step called: Arrange (Setup)
+        [SetUp]
+        public void Setup()
+        {
+            // Arrange
+            m_InventoryModel = new InventoryModel(10);
+            
+            m_PotionItem = ScriptableObject.CreateInstance<InventoryItemSO>();
+            m_PotionItem.name = "Potion";
+            m_PotionItem.MaxStack = 3;
+            
+            m_SwordItem = ScriptableObject.CreateInstance<InventoryItemSO>();
+            m_PotionItem.name = "Sword";
+            m_PotionItem.MaxStack = 5;
+        }
+
+        // The Goal: Prove that adding an item to an empty inventory works.
+        [Test]
+        public void AddNewItem_InventoryEmpty()
+        {
+            // Act
+            bool result = m_InventoryModel.TryAddItem(m_PotionItem, 1);
+            
+            // Assert
+            Assert.IsTrue(result, "TryAddItem should return true");
+            Assert.AreEqual(1, m_InventoryModel.GetSlot(0).Quantity, "Slot 0 should return 1");
+            Assert.AreEqual(m_PotionItem, m_InventoryModel.GetSlot(0).ItemData, "Slot 0 should hold item data: Potion");
+        }
+
+        // Test that adding the same item twice combines them, rather than taking a new slot.
+        [Test]
+        public void AddSameItemTwice_InventoryNotEmpty()
+        {
+            // Act
+            bool potion_1 = m_InventoryModel.TryAddItem(m_PotionItem, 1);
+            bool potion_2 = m_InventoryModel.TryAddItem(m_PotionItem, 2);
+            
+            // Assert
+            Assert.IsTrue(potion_1, "TryAddItem potion_1 should return true");
+            Assert.IsTrue(potion_2, "TryAddItem potion_2 should return true");
+            
+            Assert.AreEqual(3, m_InventoryModel.GetSlot(0).Quantity, "Slot 0 should return 3");
+            Assert.IsTrue(m_InventoryModel.GetSlot(1).IsEmpty, "Slot 1 should return true");
+        }
+
+        // Test that removing the exact amount clears the slot completely.
+        [Test]
+        public void RemoveExactAmountItem_InventoryNotEmpty()
+        {
+            // Act
+            bool potion_1 = m_InventoryModel.TryAddItem(m_PotionItem, 3);
+            
+            // Assert
+            Assert.IsTrue(potion_1, "TryAddItem potion_1 should return true");
+            
+            // Act
+            m_InventoryModel.RemoveItem(0, 3);
+            
+            // Assert
+            Assert.IsTrue(m_InventoryModel.GetSlot(0).IsEmpty, "Slot 0 should be empty after removing 3 from index 0");
+            Assert.AreEqual(0, m_InventoryModel.GetSlot(0).Quantity, "Slot 0 should return 0");
+        }
+
+        // Test Swapping two items.
+        [Test]
+        public void SwapItems_InventoryNotEmpty()
+        {
+            // Arrange
+            m_InventoryModel.TryAddItem(m_PotionItem, 1);
+            m_InventoryModel.TryAddItem(m_SwordItem, 1);
+            
+            // Act
+            m_InventoryModel.SwapSlots(0, 1);
+            
+            // Assert
+            Assert.AreEqual(m_SwordItem, m_InventoryModel.GetSlot(0).ItemData, "Slot 0 should hold sword");
+            Assert.AreEqual(m_PotionItem, m_InventoryModel.GetSlot(1).ItemData, "Slot 1 should hold potion");
+        }
+    }
+}
