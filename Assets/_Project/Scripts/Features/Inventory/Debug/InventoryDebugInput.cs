6366bc204888d1083316d662c9c57f6da7bb2f1b
Anis <>
2025-12-02 13:17:03 +0200

    Add InventoryDebug script and auto-inject it.

---
+++ Assets/_Project/Scripts/Features/Inventory/Debug/InventoryDebugInput.cs
@@ -0,0 +1,53 @@
+using System;
+using Project.Features.Inventory.Domain;
+using UnityEngine;
+using VContainer;
+
+namespace Project.Features.Inventory.DebugScripts
+{
+    public class InventoryDebugInput : MonoBehaviour
+    {
+        [Header("Test Items")] 
+        [SerializeField]  private InventoryItemSO m_Sword;
+        [SerializeField] private InventoryItemSO m_Potion;
+        
+        private InventoryModel m_InventoryModel;
+
+        [Inject]
+        public void Construct(InventoryModel inventoryModel)
+        {
+            m_InventoryModel = inventoryModel;
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                // Add 1 Sword.
+                Debug.Log("Adding Sword x1");
+                m_InventoryModel.TryAddItem(m_Sword, 1);
+            }
+
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                // Add 5 Potions (Stacking test).
+                Debug.Log("Adding Potions x5");
+                m_InventoryModel.TryAddItem(m_Potion, 5);
+            }
+
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                // Remove 1 item from Slot 0.
+                Debug.Log("Removing 1 item from slot 0");
+                m_InventoryModel.RemoveItem(0, 1);
+            }
+
+            if (Input.GetKeyDown(KeyCode.T))
+            {
+                // Remove 5 items from Slot 0.
+                Debug.Log("Removing 5 items from slot 0");
+                m_InventoryModel.RemoveItem(0, 5);
+            }
+        }
+    }
+}
