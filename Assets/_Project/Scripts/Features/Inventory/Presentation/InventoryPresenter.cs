140ec4df72f0dc5a7897cb77a3f3d9bcd9d1fd3b
Anis <>
2025-12-02 13:06:19 +0200

    Connect the Model and View in the Presenter.

--- Assets/_Project/Scripts/Features/Inventory/Presentation/InventoryPresenter.cs
+++ Assets/_Project/Scripts/Features/Inventory/Presentation/InventoryPresenter.cs
@@ -1,5 +1,6 @@
 using System;
 using Project.Features.Inventory.Domain;
+using Project.Features.Inventory.View;
 using UnityEngine;
 using VContainer.Unity;
 
@@ -8,26 +9,63 @@ namespace Project.Features.Inventory.Presentation
     public class InventoryPresenter : IStartable, IDisposable
     {
         private readonly InventoryModel m_InventoryModel;
-
-        public InventoryPresenter(InventoryModel inventoryModel)
+        private readonly InventoryView m_InventoryView;
+        
+        public InventoryPresenter(InventoryModel inventoryModel, InventoryView inventoryView)
         {
             m_InventoryModel = inventoryModel;
+            m_InventoryView = inventoryView;
         }
         
         public void Start()
         {
-            m_InventoryModel.OnSlotUpdated += InventoryModel_OnSlotUpdated;
             Debug.Log("Starting InventoryPresenter");
+            
+            m_InventoryModel.OnSlotUpdated += InventoryModel_OnSlotUpdated;
+            m_InventoryView.OnSlotClicked += InventoryView_OnSlotClicked;
+            
+            // Initialize the Grid
+            int inventoryCapacity = m_InventoryModel.Capacity;
+            m_InventoryView.InitializeUI(inventoryCapacity);
+            
+            // Update the grid if model has saved inventory items
+            for (int i = 0; i < inventoryCapacity; i++)
+            {
+                HandleSlotUpdate(i);
+            }
         }
         public void Dispose()
         {
-            m_InventoryModel.OnSlotUpdated -= InventoryModel_OnSlotUpdated;
             Debug.Log("Destroying InventoryPresenter");
+            
+            m_InventoryModel.OnSlotUpdated -= InventoryModel_OnSlotUpdated;
+            m_InventoryView.OnSlotClicked -= InventoryView_OnSlotClicked;
         }
         
-        private void InventoryModel_OnSlotUpdated(int slot)
+        private void InventoryModel_OnSlotUpdated(int slotIndex)
         {
-            Debug.Log("Slot Updated, index: " + slot);
+            Debug.Log("Slot Updated, index: " + slotIndex);
+            
+            HandleSlotUpdate(slotIndex);
+        }
+        
+        private void InventoryView_OnSlotClicked(int slotIndex)
+        {
+            Debug.Log("Slot Clicked, index: " + slotIndex);
+        }
+
+        private void HandleSlotUpdate(int index)
+        {
+            InventorySlot slot = m_InventoryModel.GetSlot(index);
+
+            if (slot.IsEmpty)
+            {
+                m_InventoryView.UpdateSlot(index, null, 0);
+            }
+            else
+            {
+                m_InventoryView.UpdateSlot(index, slot.ItemData.Icon, slot.Quantity);
+            }
         }
     }
 }
