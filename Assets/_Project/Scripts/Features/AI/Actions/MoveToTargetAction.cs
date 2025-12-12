5e7ba5b0875a9723b2861481e7d62da981f83926
Anis <>
2025-12-12 15:12:11 +0200

    Add MoveToTarget action.

---
+++ Assets/_Project/Scripts/Features/AI/Actions/MoveToTargetAction.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using Project.Features.AI.Components;
+using Project.Features.AI.Domain;
+
+namespace Project.Features.AI.Actions
+{
+    public class MoveToTargetAction : UtilityActionBase
+    {
+        private readonly NpcContext m_Context;
+
+        public MoveToTargetAction(List<IConsideration> considerations,  string name, NpcContext context) 
+            : base(considerations, "Move to Target")
+        {
+            m_Context = context;
+        }
+
+        public override void Execute()
+        {
+            m_Context.Agent.SetDestination(m_Context.Target.position);
+        }
+    }
+}
