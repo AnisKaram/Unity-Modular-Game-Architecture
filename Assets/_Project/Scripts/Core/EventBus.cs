74d7609d8059a5b86f91078030b1f7c638306fbb
Anis <>
2026-01-12 14:41:21 +0200

    Add EventBus Infrastructure.
    - Subscribe, Unsubscribe, and Raise methods have been added.

--- Assets/_Project/Scripts/Core/EventBus.cs
+++ Assets/_Project/Scripts/Core/EventBus.cs
@@ -1,19 +1,44 @@
-using UnityEngine;
+using System;
+using System.Collections.Generic;
 
 namespace Project.Core
 {
-    public class EventBus : MonoBehaviour
+    public class EventBus
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
+        private readonly Dictionary<Type, Delegate> m_Handlers = new Dictionary<Type, Delegate>();
+
+        public void Subscribe<T>(Action<T> callback)
         {
-        
+            if (!m_Handlers.ContainsKey(typeof(T))) // Add the callback if the type T isn't in the dictionary. 
+            {
+                m_Handlers.Add(typeof(T), callback); 
+            }
+            else // Combine the callback to the existing chain.
+            {
+                m_Handlers[typeof(T)] = Delegate.Combine(m_Handlers[typeof(T)], callback);
+            }
         }
 
-        // Update is called once per frame
-        void Update()
+        public void Unsubscribe<T>(Action<T> callback)
         {
-        
+            if (m_Handlers.ContainsKey(typeof(T))) // Remove the callback from the source. 
+            {
+                m_Handlers[typeof(T)] = Delegate.Remove(m_Handlers[typeof(T)], callback);
+
+                if (m_Handlers[typeof(T)] == null) // Remove the key when the list becomes null/empty.
+                {
+                    m_Handlers.Remove(typeof(T));
+                }
+            }
+        }
+
+        public void Raise<T>(T signal)
+        {
+            if (m_Handlers.ContainsKey(typeof(T)))
+            {
+                Action<T> casted = (Action<T>)m_Handlers[typeof(T)];
+                casted?.Invoke(signal);
+            }
         }
     }
 }
