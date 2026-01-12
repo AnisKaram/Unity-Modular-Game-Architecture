b534b31c507c6d81ddea1a07ed955d0354b064c6
Anis <>
2026-01-12 13:33:18 +0200

    Create EventBus core script.

---
+++ Assets/_Project/Scripts/Core/EventBus.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Project.Core
+{
+    public class EventBus : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
