8dd47334e593bd16746e267c0f136722b4c646a4
Anis <anistkaram@hotmail.com>
2026-01-13 13:36:41 +0200

    Add TimeSnapshot command.

---
+++ Assets/_Project/Scripts/Features/TimeRewind/Commands/TimeSnapshotCommand.cs
@@ -0,0 +1,36 @@
+using Project.Core.Interfaces;
+using UnityEngine;
+
+namespace Project.Features.TimeRewind.Commands
+{
+    public class TimeSnapshotCommand : ICommand
+    {
+        private readonly Transform m_Target;
+        private readonly Rigidbody m_Rigidbody;
+        private readonly Vector3 m_Position;
+        private readonly Quaternion m_Rotation;
+        private readonly Vector3 m_Velocity;
+
+        public TimeSnapshotCommand(Transform target, Rigidbody rigidbody)
+        {
+            m_Target = target;
+            m_Rigidbody = rigidbody;
+            
+            m_Position = m_Target.position;
+            m_Rotation = m_Target.rotation;
+            m_Velocity = m_Rigidbody.linearVelocity;
+        }
+        
+        public void Execute()
+        {
+            
+        }
+
+        public void Undo()
+        {
+            m_Target.position = m_Position;
+            m_Target.rotation = m_Rotation;
+            m_Rigidbody.linearVelocity = m_Velocity;
+        }
+    }
+}
