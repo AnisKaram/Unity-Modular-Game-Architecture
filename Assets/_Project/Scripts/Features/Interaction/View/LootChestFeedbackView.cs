7f81a42d33df99a46e73719b43b42fbf382102c3
Anis <anistkaram@hotmail.com>
2026-01-20 20:16:29 +0200

    Add Feedback Animation

--- Assets/_Project/Scripts/Features/Interaction/View/LootChestFeedbackView.cs
+++ Assets/_Project/Scripts/Features/Interaction/View/LootChestFeedbackView.cs
@@ -1,6 +1,7 @@
 using Project.Features.Interaction.Components;
 using TMPro;
 using UnityEngine;
+using DG.Tweening;
 
 namespace Project.Features.Interaction.View
 {
@@ -10,27 +11,37 @@ namespace Project.Features.Interaction.View
         [SerializeField] private LootChest m_LootChest;
         [SerializeField] private TextMeshPro m_FeedbackText;
 
+        [Header("Animation Settings")] 
+        [SerializeField] private float m_YOffset = 1f;
+        [SerializeField] private float m_Duration = 0.5f;
+
         private void OnEnable()
         {
             m_LootChest.OnOpen += LootChest_OnOpen;
-            HideFeedback();
+            Hide();
         }
         private void OnDisable()
         {
             m_LootChest.OnOpen -= LootChest_OnOpen;
-            HideFeedback();
+            Hide();
+            KillAnimation();
         }
         
         private void LootChest_OnOpen(object sender, LootChest.LootChestEventArgs eventArgs)
         {
-            SetFeedbackText(eventArgs.Name, eventArgs.Quantity);
-            ShowFeedback();
+            SetText(eventArgs.Name, eventArgs.Quantity);
+            Show();
+            StartAnimation();
         }
 
-        private void SetFeedbackText(string feedback, int quantity) =>
+        private void SetText(string feedback, int quantity) =>
             m_FeedbackText.text = "+" + quantity + " " + feedback;
         
-        private void ShowFeedback() => m_FeedbackText.gameObject.SetActive(true);
-        private void HideFeedback() => m_FeedbackText.gameObject.SetActive(false);
+        private void Show() => m_FeedbackText.gameObject.SetActive(true);
+        private void Hide() => m_FeedbackText.gameObject.SetActive(false);
+
+        private void StartAnimation() =>
+            m_FeedbackText.rectTransform.DOAnchorPosY(m_FeedbackText.rectTransform.anchoredPosition.y + m_YOffset, m_Duration);
+        private void KillAnimation() => m_FeedbackText.rectTransform.DOKill();
     }
 }
